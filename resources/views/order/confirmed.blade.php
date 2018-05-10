0bb0a1cbd74c059ba4a8894771f6b57feea2adac
hebingchang <hebingchang1@live.com>
2018-05-10 23:16:56 +0800

    Improve user experience

--- resources/views/order/confirmed.blade.php
+++ resources/views/order/confirmed.blade.php
@@ -10,33 +10,35 @@
             <div class="title">支付订单</div>
         </div>
     </div>
-    <div class="block-title">支付信息</div>
+    <div class="page-content">
 
-    @if ($success == false)
-        <div id="pay_success" hidden>false</div>
-        <div id="error_msg" hidden>{{ $msg }}</div>
-        <div class="card">
-            <div class="card-header"><p><i class="material-icons">close</i> 支付失败</p></div>
-            <div class="card-content card-content-padding">{{ $msg }}</div>
-            <div class="card-footer"><a class="button button-outline back" href="#" data-ignore-cache="true">返回</a>
+        <div class="block-title">支付信息</div>
+
+        @if ($success == false)
+            <div id="pay_success" hidden>false</div>
+            <div id="error_msg" hidden>{{ $msg }}</div>
+            <div class="card">
+                <div class="card-header"><p><i class="material-icons">close</i> 支付失败</p></div>
+                <div class="card-content card-content-padding">{{ $msg }}</div>
+                <div class="card-footer"><a class="button button-outline back" href="#" data-ignore-cache="true">返回</a>
+                </div>
             </div>
-        </div>
 
-    @else
-        <div id="pay_success" hidden>true</div>
-        <div class="card">
-            <div class="card-header"><p><i class="material-icons">check</i> 订单 #{{ $order->id }} 支付成功</p></div>
-            <div class="card-content card-content-padding">
-                <code>消费 ￥{{ $order->price }}，您的账户剩余 ￥{{ \Auth::user()->balance }} 元。</code>
-                <code>
-                    请牢记您的列车车次 {{ $order->train_code }}({{ $order->from_station }}-->{{ $order->to_station }})
-                    <br />{{ $order->departure_date }} {{ $order->departure_time }} 发车
-                    <br />{{ $order->seat_no }}
-                </code>
+        @else
+            <div id="pay_success" hidden>true</div>
+            <div class="card">
+                <div class="card-header"><p><i class="material-icons">check</i> 订单 #{{ $order->id }} 支付成功</p></div>
+                <div class="card-content card-content-padding">
+                    <code>消费 ￥{{ $order->price }}，您的账户剩余 ￥{{ \Auth::user()->balance }} 元。</code>
+                    <code>
+                        请牢记您的列车车次 {{ $order->train_code }}({{ $order->from_station }}-->{{ $order->to_station }})
+                        <br />{{ $order->departure_date }} {{ $order->departure_time }} 发车
+                        <br />{{ $order->seat_no }}
+                    </code>
+                </div>
+                <div class="card-footer"><a class="button button-outline back" href="#" data-ignore-cache="true">返回</a></div>
             </div>
-            <div class="card-footer"><a class="button button-outline back" href="#" data-ignore-cache="true">返回</a></div>
-        </div>
 
-    @endif
-
+        @endif
+    </div>
 </div>
--- resources/views/order/pay.blade.php
+++ resources/views/order/pay.blade.php
@@ -10,44 +10,43 @@
             <div class="title">支付订单</div>
         </div>
     </div>
-    <div class="block-title">订单信息</div>
-    <div class="list simple-list">
-        <ul>
-            <li><strong>订单 #{{ $order->id }}</strong></li>
-            <li><code>{{ $order->departure_date }} {{ $order->train_code }}({{ $order->from_station }}-->{{ $order->to_station }}): {{ $order->seat_type }} {{ $order->seat_no }}</code></li>
-            <li>应付款 ￥{{ $order->price }}</li>
-            <li id="status">{!! $order->status !!}</li>
-            <li class="row">
-                {!! $order->can_pay ? "<button id='confirm_pay' class='col-15 button button-outline button-small button-round color-green'>支付订单</button>" : "" !!}
-                {!! $order->can_cancel ? "<button id='cancel_order' class='col-15 button button-outline button-small button-round color-red'>取消订单</button>" : "" !!}
-                <a class="col-15 button button-outline button-small button-round back" href="#">返回</a>
-                <div class="col-{{ 85 - $order->can_pay * 15 - $order->can_cancel * 15 }}">
-                    <!-- 占位 -->
-                </div>
-            </li>
-        </ul>
+    <div class="page-content">
+
+        <div class="block-title">订单信息</div>
+        <div class="list simple-list">
+            <ul>
+                <li><strong>订单 #{{ $order->id }}</strong></li>
+                <li><code>{{ $order->departure_date }} {{ $order->train_code }}({{ $order->from_station }}-->{{ $order->to_station }}): {{ $order->seat_type }} {{ $order->seat_no }}</code></li>
+                <li>应付款 ￥{{ $order->price }}</li>
+                <li id="status">{!! $order->status !!}</li>
+                <li class="row">
+                    {!! $order->can_pay ? "<button id='confirm_pay' class='col-15 button button-outline button-small button-round color-green'>支付订单</button>" : "" !!}
+                    {!! $order->can_cancel ? "<button id='cancel_order' class='col-15 button button-outline button-small button-round color-red'>取消订单</button>" : "" !!}
+                    <a class="col-15 button button-outline button-small button-round back" href="#">返回</a>
+                    <div class="col-{{ 85 - $order->can_pay * 15 - $order->can_cancel * 15 }}">
+                        <!-- 占位 -->
+                    </div>
+                </li>
+            </ul>
+        </div>
+
+        <div id="can_pay" hidden>{{ $order->can_pay ? "true" : "false" }}</div>
+        <div id="can_cancel" hidden>{{ $order->can_cancel ? "true" : "false" }}</div>
+        <div id="order_id" hidden>{{ $order->id }}</div>
+
+
+        @if ($order->can_pay)
+            <div id="pay_dialog" style="display: none">
+                您目前余额 ￥{{ \Auth::user()->balance }}，票价￥{{ $order->price }}{{ \Auth::user()->balance < $order->price ? "，余额不足。" : "" }}
+                <br />确认支付？
+            </div>
+        @endif
+
+
+        @if ($order->can_cancel)
+            <div id="cancel_dialog" style="display: none">
+                确认取消订单吗？此操作将无法撤销。
+            </div>
+        @endif
     </div>
-
-    <div id="can_pay" hidden>{{ $order->can_pay ? "true" : "false" }}</div>
-    <div id="can_cancel" hidden>{{ $order->can_cancel ? "true" : "false" }}</div>
-    <div id="order_id" hidden>{{ $order->id }}</div>
-
-
-    @if ($order->can_pay)
-        <div id="pay_dialog" style="display: none">
-            您将要支付 {{ $order->departure_date }} {{ $order->train_code }}({{ $order->from_station }}-->{{ $order->to_station }}) {{ $order->seat_type }}
-            <br />
-            您目前余额 ￥{{ \Auth::user()->balance }}，票价￥{{ $order->price }}{{ \Auth::user()->balance < $order->price ? "，余额不足。" : "" }}
-        </div>
-    @endif
-
-
-    @if ($order->can_cancel)
-        <div id="cancel_dialog" style="display: none">
-            您将要取消 {{ $order->departure_date }} {{ $order->train_code }}({{ $order->from_station }}-->{{ $order->to_station }}) {{ $order->seat_type }}
-            <br />
-            确认取消订单吗？此操作将无法撤销。
-        </div>
-    @endif
-
 </div>
